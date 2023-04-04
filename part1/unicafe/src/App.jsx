c6f230d715913e4da18c371e6ce05e50b63ac6cd
jackislam191 <79713868+jackislam191@users.noreply.github.com>
2023-04-04 22:09:47 +0800

    ex 1.10
    handle button component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,56 +1,41 @@
-import { useState } from 'react'
-
-const Statistics = (props) => {
-  return (
-    <div>
-      <p>{props.statTitle} {props.statItem}</p>
-    </div>
-  )
-}
+import { useState } from "react";
+import Statistics from "./Components/Statistics";
+import Button from "./Components/Button";
 
 const App = () => {
-   // save clicks of each button to its own state
-   const [good, setGood] = useState(0)
-   const [neutral, setNeutral] = useState(0)
-   const [bad, setBad] = useState(0)
-   
-   const goodCountHandler = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodCountHandler = () => {
     setGood(good + 1);
-   }
+  };
 
-   const neutralCountHandler = () => {
+  const neutralCountHandler = () => {
     setNeutral(neutral + 1);
-   }
+  };
 
-   const badCountHandler = () => {
+  const badCountHandler = () => {
     setBad(bad + 1);
-   }
+  };
 
-  let all = (good + neutral + bad);
-  let average = (all === 0) ? 0 : (good - bad)/ all;
-  let postive = (all === 0) ? 0 : (good) / all * 100 + '%';
- 
-   return (
-     <div>
-        <h1>Give feedback</h1>
-        <button onClick={goodCountHandler}>good</button>
-        <button onClick={neutralCountHandler}>netural</button>
-        <button onClick={badCountHandler}>bad</button>
-
-        <h1>statistics</h1>
-        {(all === 0 )? <p>No feedback given</p> :
+  return (
+    <div>
+      <h1>Give feedback</h1>
+        <Button feedBackHandler={goodCountHandler} text={'good'}></Button>
+        <Button feedBackHandler={neutralCountHandler} text={'neutral'}></Button>
+        <Button feedBackHandler={badCountHandler} text={'bad'}></Button>
+      <h1>statistics</h1>
+      {(good + neutral + bad) === 0 ? (
+        <p>No feedback given</p>
+      ) : (
         <div>
-          <Statistics statTitle={'good'} statItem={good}></Statistics>
-          <Statistics statTitle={'netural'} statItem={neutral}></Statistics>
-          <Statistics statTitle={'bad'} statItem={bad}></Statistics>
-          <Statistics statTitle={'All'} statItem={all}></Statistics>
-          <Statistics statTitle={'average'} statItem={average}></Statistics>
-          <Statistics statTitle={'positive'} statItem={postive}></Statistics>
-        </div>
-        }
-        
-     </div>
-   )
-}
+          <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+        </div> 
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ part1/unicafe/src/Components/Statistics.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import StatisticLine from "./StatisticLine";
+
+const Statistics = (props) => {
+    const {good, neutral, bad} = props;
+    let all = good + neutral + bad;
+    let average = all === 0 ? 0 : (good - bad) / all;
+    let postive = all === 0 ? 0 : (good / all) * 100 + "%";
+  
+    return (
+      <div>
+        <StatisticLine text={"good"} value={good}></StatisticLine>
+        <StatisticLine text={"neutral"} value={neutral}></StatisticLine>
+        <StatisticLine text={"bad"} value={bad}></StatisticLine>
+        <StatisticLine text={"All"} value={all}></StatisticLine>
+        <StatisticLine text={"average"} value={average}></StatisticLine>
+        <StatisticLine text={"positive"} value={postive}></StatisticLine>
+      </div>
+    );
+  };
+export default Statistics;
