31d9200def7548c8c32001a8b64c207fc5855665
jackislam191 <79713868+jackislam191@users.noreply.github.com>
2023-04-05 20:01:10 +0800

    ex2.3 handle total

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Header from './Components/Header';
-import Content from './Components/Content';
-import Total from './Components/Total';
 import Course from './Components/Course';
 
 function App() {
--- part2/courseinfo/src/Components/Content.jsx
+++ part2/courseinfo/src/Components/Content.jsx
@@ -5,9 +5,13 @@ const Content = (props) => {
     const contentItem = props.parts.map((content) => 
         <Part key={content.id} part={content.name} exercise={content.exercises} />
     );
+
+    let sum = props.parts.reduce((total, currentVal) => total + currentVal.exercises, 0);
+
     return (
         <div>
             {contentItem}
+            <p>total of {sum} exercises</p>
         </div>
     );
 }
